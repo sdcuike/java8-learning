3cf71533beaf2b64e621ff9e469b864c2ef1b1f8
sdcukesdcuike@aol.com <sdcukesdcuike@aol.com>
2016-09-10 18:49:31 +0800

    枚举实现单例不能延迟加载资源，但保证了enum值只实例化一次。而且克服了反射带来的问题。 但是缺点是不灵活，而且不能懒初始化

--- src/main/java/com/doctor/design_pattern/singleton/EnumSingleton.java
+++ src/main/java/com/doctor/design_pattern/singleton/EnumSingleton.java
@@ -3,8 +3,8 @@ package com.doctor.design_pattern.singleton;
 /**
  * @author sdcuike
  *
- *         Enum Singleton
- *         枚举实现单例不能延迟加载资源，但保证了enum值只实例化一次。而且克服了反射带来的问题
+ *         Enum Singleton 枚举实现单例不能延迟加载资源，但保证了enum值只实例化一次。而且克服了反射带来的问题。
+ *         但是缺点是不灵活，而且不能懒初始化
  * 
  *         Created on 2016年8月1日 下午12:24:06
  */
