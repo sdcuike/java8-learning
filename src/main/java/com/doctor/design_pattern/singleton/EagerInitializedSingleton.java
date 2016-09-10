08cb7764c6fc6ba024425980e83e98d97114574f
sdcuke <sdcuike@aol.com>
2016-09-10 12:31:36 +0800

    克隆、反射与单例

--- src/main/java/com/doctor/design_pattern/singleton/EagerInitializedSingleton.java
+++ src/main/java/com/doctor/design_pattern/singleton/EagerInitializedSingleton.java
@@ -5,19 +5,29 @@ package com.doctor.design_pattern.singleton;
  *
  *         Created on 2016年7月31日 下午11:36:05
  * 
- *         饿汉式 单例
- *         EagerInitialized Singleton
+ *         饿汉式 单例 EagerInitialized Singleton
  * 
- *         If your singleton class is not using a lot of resources, this is the approach to use.
- *         But in most of the scenarios, Singleton classes are created for resources
- *         such as File System, Database connections etc and we should avoid
- *         the instantiation until unless client calls the getInstance
- *         method. Also this method doesn’t provide any options for exception handling.
+ *         If your singleton class is not using a lot of resources, this is the
+ *         approach to use. But in most of the scenarios, Singleton classes are
+ *         created for resources such as File System, Database connections etc
+ *         and we should avoid the instantiation until unless client calls the
+ *         getInstance method. Also this method doesn’t provide any options for
+ *         exception handling.
+ * 
+ *         饿汉式单例在类加载的时候就初始化资源，只提供一次加载资源/初始化的机会，失败导致资源不能正常初始化
  */
-public class EagerInitializedSingleton {
+public class EagerInitializedSingleton implements Cloneable {
     private static final EagerInitializedSingleton instance = new EagerInitializedSingleton();
 
+    /**
+     * 反射导致单例失败：How to fix: Throw Runtime Exception if someone tries to make
+     * instance in case one instance already exists. This code will go into the
+     * private constructor of the Singleton class
+     */
     private EagerInitializedSingleton() {
+        if (instance != null) {
+            throw new RuntimeException("plean use  getInstance() method");
+        }
     }
 
     public static EagerInitializedSingleton getInstance() {
@@ -28,7 +38,19 @@ public class EagerInitializedSingleton {
         System.out.println("test");
     }
 
-    public static void main(String[] args) {
+    /**
+     * 克隆破坏单例：How to fix: Throw CloneNotSupportedException from the clone ()
+     * method if someone tries to make other instance of it
+     */
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        if (instance != null) {
+            throw new CloneNotSupportedException("plean use  getInstance() method");
+        }
+        return super.clone();
+    }
+
+    public static void main(String[] args) throws ReflectiveOperationException, SecurityException, CloneNotSupportedException {
         System.out.println(EagerInitializedSingleton.getInstance());
         System.out.println(EagerInitializedSingleton.getInstance());
         EagerInitializedSingleton.getInstance().doSomething();
@@ -38,6 +60,19 @@ public class EagerInitializedSingleton {
 
         System.out.println(EagerInitializedSingleton.getInstance() == EagerInitializedSingleton.getInstance());
         // true
+
+        // 反射破坏
+        // Constructor<EagerInitializedSingleton> constructor =
+        // EagerInitializedSingleton.class.getDeclaredConstructor();
+        // constructor.setAccessible(true);
+        // EagerInitializedSingleton newInstance = constructor.newInstance();
+        // System.out.println(newInstance);
+
+        // 克隆破坏
+        System.out.println("克隆破坏:");
+        EagerInitializedSingleton eagerInitializedSingleton = EagerInitializedSingleton.getInstance();
+        EagerInitializedSingleton eagerInitializedSingletonClone = (EagerInitializedSingleton) eagerInitializedSingleton.clone();
+        System.out.println(eagerInitializedSingleton + "==" + eagerInitializedSingletonClone + " is " + (eagerInitializedSingleton == eagerInitializedSingletonClone));
     }
 
 }
