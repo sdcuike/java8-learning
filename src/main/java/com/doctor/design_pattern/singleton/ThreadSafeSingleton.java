bd3aa682bd618c4727ea18685dc33e55d1789771
sdcuke <sdcuike@aol.com>
2016-09-10 18:09:05 +0800

    粗粒度锁带来的单例线程安全

--- src/main/java/com/doctor/design_pattern/singleton/ThreadSafeSingleton.java
+++ src/main/java/com/doctor/design_pattern/singleton/ThreadSafeSingleton.java
@@ -1,24 +1,44 @@
 package com.doctor.design_pattern.singleton;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  * @author sdcuike
  *
  *         Created on 2016年8月1日 上午11:19:36
  * 
+ *         粗粒度锁带来的单例线程安全
+ * 
  *         Thread Safe Singleton
  * 
- *         The easier way to create a thread-safe singleton class is to make the global access
- *         method synchronized,
- *         so that only one thread can execute this method at a time.
- *         General implementation of this approach is like the below class.
+ *         The easier way to create a thread-safe singleton class is to make the
+ *         global access method synchronized, so that only one thread can
+ *         execute this method at a time. General implementation of this
+ *         approach is like the below class.
  */
 public class ThreadSafeSingleton {
 
     private static ThreadSafeSingleton instance;
 
     public static synchronized ThreadSafeSingleton getInstance() {
+        try {
+            Thread.sleep((long) (Math.random() * 100));
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         if (instance == null) {
+            try {
+                Thread.sleep((long) (Math.random() * 100));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
             instance = new ThreadSafeSingleton();
+            try {
+                Thread.sleep((long) (Math.random() * 100));
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
         return instance;
     }
@@ -28,8 +48,15 @@ public class ThreadSafeSingleton {
     }
 
     public static void main(String[] args) {
-        System.out.println(ThreadSafeSingleton.getInstance() == ThreadSafeSingleton.getInstance());
-        // true
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        for (int i = 0; i < 10; i++) {
+            executorService.submit(() -> {
+                System.out.println(ThreadSafeSingleton.getInstance());
+            });
+        }
+
+        executorService.shutdown();
     }
 
 }
